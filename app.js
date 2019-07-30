0504e708477037ecdd2b6732cc07fb7824d8f7c2
Nicki Nixon <nickitza@gmail.com>
2019-07-30 11:31:20 -0600

    pass .ejs from app to .ejs file

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', function(req, res){
   res.render('home.ejs')
 })
 
+app.get('/fallinlovewith/:thing', function(req, res){
+  var thing = req.params.thing
+  res.render('love.ejs', {thingVar: thing})
+})
+
 app.listen(3000, function(){
   console.log("mystery science port 3000")
 })
