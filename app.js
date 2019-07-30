b2fc7a75056e56f56d45543a016b16d3f9da3ecb
Nicki Nixon <nickitza@gmail.com>
2019-07-30 16:48:11 -0600

    link css, make partials

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express')
 var app = express()
+app.use(express.static("public"))
+//how to link assets in the public folder to the app
+app.set("view engine", "ejs")
+//lets express know our view pages will be .ejs files, can leave the .ejs ext off
 
 app.get('/', function(req, res){
-  res.render('home.ejs')
+  res.render('home')
 })
 
 app.get('/fallinlovewith/:thing', function(req, res){
   var thing = req.params.thing
-  res.render('love.ejs', {thingVar: thing.toLowerCase()})
+  res.render('love', {thingVar: thing.toLowerCase()})
 })
 
 app.get('/posts', function(req, res){
@@ -16,7 +20,7 @@ app.get('/posts', function(req, res){
     {title: "Aliens R Cool", author: "Nicki"},
     {title: "Machines", author: "Eric"},
   ]
-  res.render('posts.ejs', { posts: posts })
+  res.render('posts', { posts: posts })
 })
 app.listen(3000, function(){
   console.log("mystery science port 3000")
