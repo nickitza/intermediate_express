7e52ac28460ec0c424c70d220d06a76610fe7701
Nicki Nixon <nickitza@gmail.com>
2019-07-30 11:57:31 -0600

    add posts

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ app.get('/', function(req, res){
 
 app.get('/fallinlovewith/:thing', function(req, res){
   var thing = req.params.thing
-  res.render('love.ejs', {thingVar: thing})
+  res.render('love.ejs', {thingVar: thing.toLowerCase()})
 })
 
+app.get('/posts', function(req, res){
+  var posts = [
+    {title: "Hissing", author: "Sarah"},
+    {title: "Aliens R Cool", author: "Nicki"},
+    {title: "Machines", author: "Eric"},
+  ]
+  res.render('posts.ejs', { posts: posts })
+})
 app.listen(3000, function(){
   console.log("mystery science port 3000")
 })
